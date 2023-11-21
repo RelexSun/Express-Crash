be88e9867ea6030684fdf52b916880971f9763b1
RelexSun <relexsunnop@gmail.com>
2023-11-21 15:41:15 +0700

    create user route

--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,26 @@
 const express = require("express");
 const router = express.Router();
+
+router.use(logger);
+
 router.get("/", (req, res) => {
+  console.log(req.query.name);
   res.send("User list");
 });
 
 router.get("/new", (req, res) => {
-  res.send("User new form");
+  res.render("users/new");
 });
 
 router.post("/", (req, res) => {
-  res.send("Create Users");
+  const isValid = true;
+  if (isValid) {
+    users.push({ firstName: req.body.firstName });
+    res.redirect(`/users/${users.length - 1}`);
+  } else {
+    console.log("Error");
+    res.render("user/new", { firstName: req.body.firstName });
+  }
 });
 
 router
@@ -34,6 +45,11 @@ router.param("id", (req, res, next, id) => {
   req.user = users[id];
   next();
 });
+
+function logger(req, res, next) {
+  console.log(req.originalUrl);
+  next();
+}
 
 // -------- The same as the code above --------
 // router.get("/:id", (req, res) => {
--- server.js
+++ server.js
@@ -1,19 +1,14 @@
 const express = require("express");
 const app = express();
 
-app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get("/", logger, (req, res) => {
-  res.render("index", { text: "World" });
-});
+app.set("view engine", "ejs");
 
 const userRouter = require("./routes/user");
 
 app.use("/users", userRouter);
-
-function logger(req, res, next) {
-  console.log(req.originalUrl);
-  next();
-}
 
 app.listen(3001);
